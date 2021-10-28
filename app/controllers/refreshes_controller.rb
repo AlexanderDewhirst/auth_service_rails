e7ee71eab33fc437e34b2edee10c932353f244a5
Alex Dewhirst <alexander.b.dewhirst@gmail.com>
2021-10-27 19:34:43 -0600

    style: Use method instead of instance variable

--- app/controllers/refreshes_controller.rb
+++ app/controllers/refreshes_controller.rb
@@ -5,7 +5,7 @@ class RefreshesController < ApplicationController
   def create
     token = get_token(request.headers)
 
-    access_token, refresh_token = Jwt::Refresher.new(token: token, user: @current_user, req: build_uri(request: request)).call
+    access_token, refresh_token = Jwt::Refresher.new(token: token, user: current_user, req: build_uri(request: request)).call
     if access_token && refresh_token
       render json: { access_token: access_token, refresh_token: refresh_token }
     else
